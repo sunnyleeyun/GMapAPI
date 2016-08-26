f6746c37bbc93bdee042ba001b23724617207d07
sunnyleeyun <sunnylee945@gmail.com>
2016-08-26 18:58:40 +0800

    first version of GMap API. There are some errors about the code.

--- GMapSwift/ViewController.swift
+++ GMapSwift/ViewController.swift
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class ViewController: UIViewController {
-
+    
+    var placesClient: GMSPlacesClient?
+    
+    // Instantiate a pair of UILabels in Interface Builder
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        placesClient = GMSPlacesClient()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // Add a UIButton in Interface Builder to call this function
+    @IBAction func getCurrentPlace(sender: UIButton) {
+        
+        placesClient?.currentPlaceWithCallback({
+            (placeLikelihoodList: GMSPlaceLikelihoodList?, error: NSError?) -> Void in
+            if let error = error {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            self.nameLabel.text = "No current place"
+            self.addressLabel.text = ""
+            
+            if let placeLicklihoodList = placeLikelihoodList {
+                let place = placeLicklihoodList.likelihoods.first?.place
+                if let place = place {
+                    self.nameLabel.text = place.name
+                    self.addressLabel.text = place.formattedAddress.componentsSeparatedByString(", ")
+                        .joinWithSeparator("\n")
+                }
+            }
+        })
     }
-
-
 }
 
+
+
+
+
+/*
+import UIKit
+import GoogleMaps
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var camera = GMSCameraPosition.cameraWithLatitude(-33.86, longitude: 151.20, zoom: 6)
+        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        self.view = mapView
+        
+        var marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+    }
+}
+*/
